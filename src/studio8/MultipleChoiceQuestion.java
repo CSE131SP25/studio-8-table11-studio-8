b7346eca0fe43490eeaf5199bf2a81e5ef0a7c6f
stellagordon <stellagordon@localhost>
2025-04-02 14:17:51 -0500

    Studio8-table11

--- src/studio8/MultipleChoiceQuestion.java
+++ src/studio8/MultipleChoiceQuestion.java
@@ -3,6 +3,11 @@ package studio8;
 import support.cse131.NotYetImplementedException;
 
 public class MultipleChoiceQuestion extends Question {
+	
+	private String prompt;
+	private String answer;
+	private int points;
+	private String[] choices;
 
 	/**
 	 * Constructor
@@ -15,15 +20,25 @@ public class MultipleChoiceQuestion extends Question {
 		// Call the super class constructor, then create and set
 		// instance variables for any values that aren't handled
 		// by the base class
-		throw new NotYetImplementedException();
+		super(prompt, answer, points);
+		this.choices = choices;
+		//throw new NotYetImplementedException();
 	}
 	
 	/**
 	 * Display the prompt for the question in addition to 
 	 * the choices present for the question.
 	 */
+	@Override
 	public void displayPrompt() {
-		throw new NotYetImplementedException();
+		super.displayPrompt();
+		
+		for (int i = 0; i < choices.length; i++){
+		
+			System.out.println((i+1) + choices[i]);
+			
+		}
+		// = this.choices; // throw new NotYetImplementedException();
 	}
 	
 	/**
@@ -31,7 +46,7 @@ public class MultipleChoiceQuestion extends Question {
 	 * @return String[] of choices
 	 */
 	public String[] getChoices() {
-		throw new NotYetImplementedException();
+		return choices;
 	}
 	
 	public static void main(String[] args) {
